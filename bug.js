ff42915145cee96e3c33e708b603cf1f7665fd11
BugHunter-X <askzenai@gmail.com>
2025-01-01 03:22:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,27 @@
+```javascript
+//pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <Link href="/about">
+        <a>Go to About</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+//pages/about.js
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </div>
+  );
+}
+```
